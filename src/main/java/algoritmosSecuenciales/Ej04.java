22b670f5ef04bbd3aa00cba6b39c1933d3f4cc96
imad <imadelfahssi@gmail.com>
2021-09-29 10:40:00 +0200

    subida desde casa, ejercicios terminados

---
+++ src/main/java/algoritmosSecuenciales/Ej04.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosSecuenciales;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MSI
+ */
+public class Ej04 {
+    public static void main(String[] args) {
+                //Declaracion de variables 
+        double gramosUsuario;
+        //La variable gramoEnLibra equivale al valor real de 1 gramo en libras
+        final double gramoEnLibra = 0.00220462;
+        //En esta variable guardaremos la cantidad de libras que equivalen los gramos
+        double cantidadConvertida;
+        //Declaracion del Scanner
+        Scanner teclado = new Scanner(System.in);
+        //Mensaje que se muestra por panatalla informativo
+        System.out.println("Introduce la cantidad de gramos que ha traido el usuario");
+        gramosUsuario = teclado.nextDouble();
+        //Guardamos en cantidadConvertida la multiplicacion de los gramos por el valor de cada gramo en libras
+        cantidadConvertida = gramosUsuario * gramoEnLibra;
+        //Mostramos por pantalla la cantidad antes de ser convertida y despues
+        System.out.println("La cantidad introducida es " + gramosUsuario + " gramos "
+                + "que equivale a " + cantidadConvertida + " libras");
+
+    }
+}
---
+++ src/main/java/algoritmosSecuenciales/Ej05.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosSecuenciales;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MSI
+ */
+public class Ej05 {
+
+    public static void main(String[] args) {
+        //Declaracion de variables 
+        final double cobroFijo = 150; //Euros
+        final double precioKmRecorrido = 0.153; //Centimos
+        
+        double cantidadTotalViaje;
+        //En esta variable guardaremos la cantidad de km que introduce el usuario
+        double kilometrosPorRecorrer;
+        //Declaracion del Scanner
+        Scanner teclado = new Scanner(System.in);
+        //Mensaje que se muestra por panatalla informativo
+        System.out.println("Introduce los kilometros que va a realiar el usuario");
+        kilometrosPorRecorrer = teclado.nextDouble();
+        //Guardamos en cantidadTotalViaje la multiplicacion de los km por el precio por km  y le sumamos la cantidad fija
+        cantidadTotalViaje = (precioKmRecorrido * kilometrosPorRecorrer) + cobroFijo;
+        //Mostramos por pantalla la cantidad antes de ser convertida y despues
+        System.out.println("Los km introducidos son " + kilometrosPorRecorrer + " km "
+                + " el servicio cuesta " + cantidadTotalViaje + " euros");
+
+    }
+}
